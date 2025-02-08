5e27f1de5ce28aba4b444e546432589cb16ae4e3
ak375456 <ak375456@gmail.com>
2025-02-08 22:40:13 +0500

    wanted to show image captured in camera Preview to show that in firstScreen but there is some bug, will fix tomorrow

--- app/src/main/java/com/example/imageapi/home_screens/presentation/screen/CameraScreen.kt
+++ app/src/main/java/com/example/imageapi/home_screens/presentation/screen/CameraScreen.kt
@@ -18,16 +18,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -53,7 +49,7 @@ fun CameraScreen(navHostController: NavHostController) {
             }
         }
         val viewModel = viewModel<CameraViewModel>()
-        val bitmap by viewModel.bitmap.collectAsState()
+
 
         Box(modifier = Modifier
             .fillMaxSize()
